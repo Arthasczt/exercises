ab2429d990a7433cbd3e9a50771f886391bf36b2
chenzitian <719645162@qq.com>
2022-08-24 14:09:06 +0800

    first commit

---
+++ clippy/target/debug/deps/clippy3-8878adad544a782b.d
@@ -0,0 +1,7 @@
+C:\Users\71964\Desktop\rustlings-main\exercises\clippy\target\debug\deps\clippy3-8878adad544a782b.rmeta: clippy3.rs
+
+C:\Users\71964\Desktop\rustlings-main\exercises\clippy\target\debug\deps\clippy3-8878adad544a782b.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
